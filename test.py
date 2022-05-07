74ab1217749c03e72f73969b870895b496bfa487
JENNY-XU2002 <83483452+JENNY-XU2002@users.noreply.github.com>
2022-05-07 09:10:00 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
-from db.models import Student
-student = Student('ljl', '0121912011004', 20, 'whut')
-print(student.get_info())
+from db.db_handler import Info
+#---使用方法---
+info = Info('info.json')
 
+#注册
+#print(info.create('jhjhjhjh','12333','12333'))
 
+#判断是否符合登陆条件
+#print(info.T('www','eeeee'))
+#print(info.T('jhjhjhjh','123'))
+
+#修改密码
+#print(info.update('jhjhjhjh','sjsjsjsjsjs'))

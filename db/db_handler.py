4306d2b85a0f00676a98e8313f56212612be05b0
JENNY-XU2002 <83483452+JENNY-XU2002@users.noreply.github.com>
2022-05-07 09:08:13 +0800

    Update db_handler.py

--- db/db_handler.py
+++ db/db_handler.py
@@ -2,7 +2,7 @@ import json
 
 class Info():
 
-    #注册及登录信息文件的创建
+    #注册及登录信息文件的读取
     def __init__(self, json_path):
         with open(json_path) as f:
             info = json.load(f)  # 将json格式数据转换为字典
@@ -25,28 +25,47 @@ class Info():
                 else:
                     return False
 
-
-    #修改数据
-    def update(self, json_path):
-        """Loads parameters from json file"""
-        with open(json_path) as f:
+    #注册，将数据存入info.json
+    def create(self,name,password1,password2):
+        with open('info.json') as f:
             info = json.load(f)
-            self.__dict__.update(info)
+        if name in info:
+            return '该用户已存在！'
+        else:
+            if password1!=password2:
+                return '密码不一致！'
+            else:
+                info[name]=password1
+                json_str = json.dumps(info)
+                with open('info.json', 'w') as f1:
+                    f1.write(json_str)
+                return '创建成功！'
+
+    #修改密码
+    def update(self, name, password):
+        with open('info.json') as f:
+            info = json.load(f)
+        if name in info:
+            info[name]=password
+            json_str = json.dumps(info)
+            with open('info.json', 'w') as f1:
+                f1.write(json_str)
+            return '修改成功'
+        else:
+            return '姓名错误！'
 
     @property  # Python内置的@property装饰器就是负责把一个方法变成属性调用的
     def dict(self):
-        """Gives dict-like access to Params instance by `params.dict['learning_rate']"""
         return self.__dict__
 
-
+'''
 if __name__ == '__main__':
-    information = {'ljl':'123'}
+    information = {'ljl':'123'}#输入的姓名和密码
     json_str = json.dumps(information, indent=4)
 
     with open('info.json', 'w') as f:  # 创建一个info.json文件
         f.write(json_str)  # 将json_str写到文件中
-
-    print(Info.T('n','ljl','123'))
     info = Info('info.json')
-    info.Name = 'ttt'   # 修改json中的数据
-    info.save('info.json')  # 将修改后的数据保存
+    info.T('ljl','123')
+    info.create('hjshdjs','sadj','sadj')
+'''
